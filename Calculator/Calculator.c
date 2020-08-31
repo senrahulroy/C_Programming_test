8e50eeb52800a7937e50b471fd7cd768e8f0db9e
rahul <nairahul1997@gmail.com>
2020-08-31 22:31:23 +0530

    Test code to understand vim and git

---
+++ Calculator/Calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+///#include "Calculator.h"
+
+///Move to .h file
+///int funcToAdd(int ,int);
+
+///function to add
+int funcToAdd(int a, int b) {
+	int sum;
+	return sum;
+}
+
+int main() {
+	printf("hello world\n");
+	return 0;
+}
